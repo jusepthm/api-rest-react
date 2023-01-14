89987e09e03da3bff4ee172842821871ffb969ee
jusepthm <jusepth.dev@gmail.com>
2023-01-13 20:22:19 -0500

    creando navegacion

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./assetss/css/App.css";
 import "bootstrap/dist/css/bootstrap.css";
 import Login from './components/Login'
-import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <React.Fragment>
       <Router>
         <Switch>
-          <Route path="/" exact>
-            <Login />
-          </Route>
+          <Route path="/" exact render ={ props=> ( <Login {...props}/>)}></Route>
+          <Route path="/dashboard" exact render ={ props=> ( <Dashboard {...props}/>)}></Route>
         </Switch>
       </Router>
     </React.Fragment>
